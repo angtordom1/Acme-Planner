82e6f527c0b9cf942e6ac0e5a237ec12448cc695
Antonio Viñuelas <antoniovinper1@gmail.com>
2021-04-05 16:52:43 +0200

    TASK 007: Finalizada
    Ahora funciona tras corregir los fallos

--- src/main/java/acme/features/authenticated/task/AuthenticatedTaskController.java
+++ src/main/java/acme/features/authenticated/task/AuthenticatedTaskController.java
@@ -3,12 +3,16 @@ package acme.features.authenticated.task;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import acme.entities.tasks.Task;
 import acme.framework.components.BasicCommand;
 import acme.framework.controllers.AbstractController;
 import acme.framework.entities.Authenticated;
 
+@Controller
+@RequestMapping("/authenticated/task")
 public class AuthenticatedTaskController extends AbstractController<Authenticated, Task>{
 	
 	// Internal state ------------------------------------------------------
